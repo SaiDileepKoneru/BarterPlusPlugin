d6174ce2f95783d9903310cc8dcd99b707c00b19
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2023-01-20 10:50:58 -0500

    updates

--- src/main/resources/barterDb.sql
+++ src/main/resources/barterDb.sql
@@ -1,11 +1,11 @@
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `barterplus` DEFAULT CHARACTER SET utf8 ;
+USE `barterplus` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`participants`
+-- Table `barterplus`.`participants`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`participants` (
+CREATE TABLE IF NOT EXISTS `barterplus`.`participants` (
   `player_uuid` VARCHAR(100) NOT NULL,
   `username` VARCHAR(45) NULL,
   `firstName` VARCHAR(45) NULL,
@@ -15,9 +15,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`participants` (
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`barterGame`
+-- Table `barterplus`.`barterGame`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`barterGame` (
+CREATE TABLE IF NOT EXISTS `barterplus`.`barterGame` (
   `num` INT NOT NULL,
   `duration` INT NULL,
   `winner` INT NULL,
@@ -25,15 +25,15 @@ CREATE TABLE IF NOT EXISTS `mydb`.`barterGame` (
   INDEX `winn_idx` (`winner` ASC) VISIBLE,
   CONSTRAINT `winn`
     FOREIGN KEY (`winner`)
-    REFERENCES `mydb`.`participants` (`player_uuid`)
+    REFERENCES `barterplus`.`participants` (`player_uuid`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`TradeRequest`
+-- Table `barterplus`.`TradeRequest`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`TradeRequest` (
+CREATE TABLE IF NOT EXISTS `barterplus`.`TradeRequest` (
   `RequestID` VARCHAR(100) NOT NULL,
   `requester` VARCHAR(100) NOT NULL,
   `requested` VARCHAR(100) NOT NULL,
@@ -46,29 +46,29 @@ CREATE TABLE IF NOT EXISTS `mydb`.`TradeRequest` (
   INDEX `barterG_idx` (`game` ASC) VISIBLE,
   CONSTRAINT `players`
     FOREIGN KEY (`requester` , `requested`)
-    REFERENCES `mydb`.`participants` (`player_uuid` , `player_uuid`)
+    REFERENCES `barterplus`.`participants` (`player_uuid` , `player_uuid`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `barterG`
     FOREIGN KEY (`game`)
-    REFERENCES `mydb`.`barterGame` (`num`)
+    REFERENCES `barterplus`.`barterGame` (`num`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Materials`
+-- Table `barterplus`.`Materials`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Materials` (
+CREATE TABLE IF NOT EXISTS `barterplus`.`Materials` (
   `Namespace` VARCHAR(30) NOT NULL,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`Namespace`));
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Exchanges`
+-- Table `barterplus`.`Exchanges`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Exchanges` (
+CREATE TABLE IF NOT EXISTS `barterplus`.`Exchanges` (
   `exchange_num` INT NOT NULL AUTO_INCREMENT,
   `TradeRequest_RequestID` VARCHAR(100) NOT NULL,
   `Materials_Namespace` VARCHAR(30) NOT NULL,
@@ -79,29 +79,29 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Exchanges` (
   INDEX `fk_Trade_has_Materials_TradeRequest1_idx` (`TradeRequest_RequestID` ASC) VISIBLE,
   CONSTRAINT `fk_Trade_has_Materials_Materials1`
     FOREIGN KEY (`Materials_Namespace`)
-    REFERENCES `mydb`.`Materials` (`Namespace`)
+    REFERENCES `barterplus`.`Materials` (`Namespace`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Trade_has_Materials_TradeRequest1`
     FOREIGN KEY (`TradeRequest_RequestID`)
-    REFERENCES `mydb`.`TradeRequest` (`RequestID`)
+    REFERENCES `barterplus`.`TradeRequest` (`RequestID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`profession`
+-- Table `barterplus`.`profession`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`profession` (
+CREATE TABLE IF NOT EXISTS `barterplus`.`profession` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`players`
+-- Table `barterplus`.`players`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`players` (
+CREATE TABLE IF NOT EXISTS `barterplus`.`players` (
   `players_uuid` VARCHAR(100) NOT NULL,
   `barterGame_num` INT NOT NULL,
   `profession` INT NULL,
@@ -111,17 +111,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`players` (
   INDEX `prof_idx` (`profession` ASC) VISIBLE,
   CONSTRAINT `fk_players_has_barterGame_players1`
     FOREIGN KEY (`players_uuid`)
-    REFERENCES `mydb`.`participants` (`player_uuid`)
+    REFERENCES `barterplus`.`participants` (`player_uuid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_players_has_barterGame_barterGame1`
     FOREIGN KEY (`barterGame_num`)
-    REFERENCES `mydb`.`barterGame` (`num`)
+    REFERENCES `barterplus`.`barterGame` (`num`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `prof`
     FOREIGN KEY (`profession`)
-    REFERENCES `mydb`.`profession` (`id`)
+    REFERENCES `barterplus`.`profession` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
